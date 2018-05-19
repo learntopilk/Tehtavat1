f2bc1be8f6ad395b340376e42ff1f1fcc0bcfe5d
learntopilk <jonsterivilja@gmail.com>
2018-05-19 09:46:36 +0300

    Tehtävä 2 tehty (Oikeasti!)§
    Aikaisemman kohdalla oltiinkin tehty vain tehtävä 1, nyt 2 oikeasti tehtynä.

--- osa1_teht1/src/index.js
+++ osa1_teht1/src/index.js
@@ -12,8 +12,19 @@ const Otsikko = (props) => {
 const Sisalto = (props) => {
     return (
     <div>
-        <p>{props.osa} {props.tehtavia}</p>
+        <Osa osa={props.osa1} tehtavia={props.tehtavia1}/>
+        <Osa osa={props.osa2} tehtavia={props.tehtavia2}/>
+        <Osa osa={props.osa3} tehtavia={props.tehtavia3}/>
+        
     </div>
+    )
+}
+
+const Osa = (props) => {
+    return (
+        <div>
+            <p>{props.osa} {props.tehtavia}</p>
+        </div>
     )
 }
 
@@ -37,10 +48,8 @@ const App = () => {
   return (
     <div>
        <Otsikko kurssi={kurssi}/> 
-       <Sisalto osa = {osa1} tehtavia = {tehtavia1}/>
-       <Sisalto osa = {osa2} tehtavia = {tehtavia2}/>
-       <Sisalto osa = {osa3} tehtavia = {tehtavia3}/>
-       <Yhteensa tehtavia = {tehtavia1 + tehtavia2 + tehtavia3} />
+       <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3}/>
+       <Yhteensa tehtavia={tehtavia1 + tehtavia2 + tehtavia3}/>
     </div>
   )
 }
