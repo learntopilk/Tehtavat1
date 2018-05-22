8a6f8b1c02aca4f7a01952a86e39579bf94bda3c
learntopilk <jonsterivilja@gmail.com>
2018-05-22 22:09:11 +0300

    Tehtävä 1.8 tehty
    Tehtävä 1.8 tehty. Sivu jaettu komponentteihin.

--- osa1_unicafe/src/index.js
+++ osa1_unicafe/src/index.js
@@ -8,6 +8,41 @@ const ClickButton = (props) => {
         <button onClick={this.handleClick(props.label)}>{props.buttonText}</button>
     )
 }*/
+const Button = ({ handleClick, label }) => {
+    return (
+        <button onClick={handleClick}>{label}</button>
+    )
+}
+
+const Statistics = (props) => {
+
+    return (
+        <div className="statistics">
+            <h5>Statistiikkaa Statistics-komponentista!</h5>
+            <Statistic label="Tykänneitä" value={props.props.good} />
+            <Statistic label="Semityytyväisiä" value={props.props.ok} />
+            <Statistic label="Ei-tykänneitä" value={props.props.bad} />
+            <Statistic label="Keskiarvo" value={props.props.ka} />
+            <Statistic label="Positiivisia" value={props.props.pos} />
+        </div>
+    )
+}
+
+const Statistic = ({ label, value }) => {
+    if (label === "Positiivisia") {
+        return (
+            <div>
+                <span>{label}: </span><span>{value}%</span>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <span>{label}: </span><span>{value}</span>
+            </div>
+        )
+    }
+}
 
 class Unicafe extends React.Component {
 
@@ -17,78 +52,55 @@ class Unicafe extends React.Component {
             votes: 0,
             good: 0,
             bad: 0,
-            ok: 0
+            ok: 0,
+            ka: 0.00,
+            pos: 0.00
         }
+    }
+    updateStats = () => {
+            this.setState({
+                ka: ((this.state.good + (this.state.bad * (-1))) / this.state.votes).toFixed(2),
+                pos: ((this.state.good / (this.state.votes)) * 100).toFixed(2)
+            })
     }
 
     handleClickGood = () => {
-        console.log("log");
-        console.log(this);
-        this.setState({ votes: this.state.votes + 1 });
-        this.setState({ good: this.state.good + 1})
-        return true;
+        this.setState({
+            votes: this.state.votes + 1,
+            good: this.state.good + 1,
+        }, this.updateStats)
     }
 
     handleClickOK = () => {
-        console.log("log");
-        console.log(this);
-        this.setState({ votes: this.state.votes + 1 });
-        this.setState({ ok: this.state.ok + 1})
-        return true;
+        this.setState({
+            votes: this.state.votes + 1,
+            ok: this.state.ok + 1,
+        }, this.updateStats)
     }
 
     handleClickBad = () => {
-        console.log("log");
-        console.log(this);
-        this.setState({ votes: this.state.votes + 1 });
-        this.setState({ bad: this.state.bad + 1})
-        return true;
+        this.setState({
+            votes: this.state.votes + 1, 
+            bad: this.state.bad + 1,
+        }, this.updateStats)
     }
 
-
     render() {
-        let ka = 0.00;
-        let pos = 0.00;
-        if (this.state.votes !== 0) {
-            ka = ((this.state.good + (this.state.bad * (-1))) / this.state.votes).toFixed(2)
-            pos = Math.round((this.state.good / (this.state.votes)) * 100).toFixed(2)
-        }
-
-
-
         return (
             <div>
                 <div>
                     <h5>Miltä maistui?</h5>
-                    <button onClick={this.handleClickGood}>Hyvältä</button>
-                    <button onClick={this.handleClickOK}>Iha jees oli</button>
-                    <button onClick={this.handleClickBad}>Pahalta</button>
+                    <Button handleClick={this.handleClickGood} label="Hyvältä" />
+                    <Button handleClick={this.handleClickOK} label="Iha jees oli" />
+                    <Button handleClick={this.handleClickBad} label="Kauheeta huonoo" />
                 </div>
                 <div>
-                    <h5>Statistiikkaaaa</h5>
-                    <div>
-                        <span>Tykänneitä: </span><span>{this.state.good}</span>
-                    </div>
-                    <div>
-                        <span>Semityytyväisiä: </span>{this.state.ok}<span></span>
-                    </div>
-                    <div>
-                        <span>Ei-tykänneitä: </span><span>{this.state.bad}</span>
-                    </div>
-                    <div>
-                        <span>Keskiarvo: </span><span>{ka}</span>
-                    </div>
-                    <div>
-                        <span>Positiivisia: </span><span>{pos}%</span>
-                    </div>
+                    <Statistics props={this.state} />
                 </div>
             </div>
         )
     }
 }
-/*                    <ClickButton label="good" buttonText="Hyvä" />
-                                    <ClickButton label="ok" buttonText="Ok" />
-                                    <ClickButton label="bad" buttonText="Huono" />*/
 
 ReactDOM.render(<Unicafe />, document.getElementById('root'));
 
